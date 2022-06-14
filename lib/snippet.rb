7e81cb3126306ca8f772f5fe6e61fdfb41c3cb83
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-06-14 17:11:11 +0100

    working tests

--- lib/snippet.rb
+++ lib/snippet.rb
@@ -1,5 +1,11 @@
 #A method called make_snippet that takes a string as an argument and returns the first five words and then a '...' if there are more than that.
 def make_snippet(str)
+ str_arr =  str.split(" ")
   
+  if str_arr.size > 5
+    return str_arr[0..4].join(" ")+ " ..."
+  end
   
+  str
 end 
+
