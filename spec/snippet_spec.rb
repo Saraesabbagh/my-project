1a85b900539f66a33d1a47a0305477d5cb5896a3
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-06-14 17:04:03 +0100

    snippet

--- spec/snippet_spec.rb
+++ spec/snippet_spec.rb
@@ -1,1 +1,11 @@
-require 'Make snippet'
+require 'snippet'
+
+RSpec.describe "make_snippet method" do
+  context "If the string has  > 5 words" do
+    it "returns the first five words then '...'" do
+      snippet = make_snippet("Today I went on a walk")
+      expect(snippet).to eq "Today I went on a ..."
+    end
+  end
+end
+
